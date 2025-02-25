aba594582a37efce205ed04d596694f586068235
Luigi Minardi <lugminsinlag@gmail.com>
2025-02-25 16:46:26 +0100

    chore: Trying an approach of reindexing everything on split_nodes_delimiter, dont seem to work, going to other approach instead.

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,5 @@
-from ast import Del
 from collections.abc import Sequence
 from enum import Enum
-from typing import Any, TypedDict
 from htmlnode import HTMLNode
 from leafnode import LeafNode
 from textnode import TextNode, TextType
@@ -23,7 +21,9 @@ def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
         case TextType.IMAGE:
             return LeafNode("", "img", {"src": text_node.url, "alt": text_node.text})
         case _:
-            raise NotImplementedError(f'TextNode of TextType "{text_node.text_type}" has not been implemented yet.')
+            raise NotImplementedError(
+                f'TextNode of TextType "{text_node.text_type}" has not been implemented yet.'
+            )
 
 
 class Delimiters(Enum):
@@ -36,7 +36,9 @@ class Delimiters(Enum):
     LINK_CLOSE = ")"
 
 
-def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_type: TextType) -> Sequence[TextNode]:
+def split_nodes_delimiter(
+    old_nodes: Sequence[TextNode], delimiter: str, text_type: TextType
+) -> Sequence[TextNode]:
     delimiters_debug: dict[str, list[int]] = {
         Delimiters.CODE.value: [],
         Delimiters.ITALIC.value: [],
@@ -44,7 +46,7 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
         Delimiters.LINK.value: [],
         Delimiters.IMAGE.value: [],
         Delimiters.LINK_MID.value: [],
-        Delimiters.LINK_CLOSE.value: []
+        Delimiters.LINK_CLOSE.value: [],
     }
 
     for node in old_nodes:
@@ -56,18 +58,18 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
                 if (
                     node.text[i] == Delimiters.ITALIC.value
                     and node.text[i + 1] == Delimiters.ITALIC.value
-                    and i-1 not in delimiters_debug[Delimiters.BOLD.value]
+                    and i - 1 not in delimiters_debug[Delimiters.BOLD.value]
                 ):
                     delimiters_debug[Delimiters.BOLD.value].append(i)
                 elif (
                     node.text[i] == Delimiters.ITALIC.value
-                    and i-1 not in delimiters_debug[Delimiters.BOLD.value]
+                    and i - 1 not in delimiters_debug[Delimiters.BOLD.value]
                 ):
                     delimiters_debug[Delimiters.ITALIC.value].append(i)
 
                 if node.text[i] == "!" and node.text[i + 1] == Delimiters.LINK.value:
                     delimiters_debug[Delimiters.IMAGE.value].append(i)
-                
+
                 if node.text[i] == Delimiters.LINK.value and node.text[i - 1] != "!":
                     delimiters_debug[Delimiters.LINK.value].append(i)
 
@@ -77,12 +79,14 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
             if node.text[i] == Delimiters.LINK_CLOSE.value:
                 delimiters_debug[Delimiters.LINK_CLOSE.value].append(i)
 
-
-            if i == len(node.text) -1:
-                if node.text[i] == Delimiters.ITALIC.value and node.text[i-1] != Delimiters.ITALIC.value:
+            if i == len(node.text) - 1:
+                if (
+                    node.text[i] == Delimiters.ITALIC.value
+                    and node.text[i - 1] != Delimiters.ITALIC.value
+                ):
                     delimiters_debug[Delimiters.ITALIC.value].append(i)
 
-    print(delimiters_debug)
+    print("\nAll delimiters:", delimiters_debug)
 
     delimiters_left: dict[str, list[int]] = {
         Delimiters.CODE.value: [],
@@ -97,20 +101,21 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
         Delimiters.ITALIC.value: [],
         Delimiters.BOLD.value: [],
         Delimiters.LINK_MID.value: [],
-        Delimiters.LINK_CLOSE.value: []
+        Delimiters.LINK_CLOSE.value: [],
     }
 
+    res: Sequence[TextNode] = []
 
     for node in old_nodes:
-        #i = 0
-        #while i < len(node.text):
         for i in range(len(node.text)):
 
             if (
                 node.text[i] == Delimiters.CODE.value
                 and (
-                    len(delimiters_left[Delimiters.CODE.value]) < len(delimiters_right[Delimiters.CODE.value])
-                    or len(delimiters_left[Delimiters.CODE.value]) == len(delimiters_right[Delimiters.CODE.value])
+                    len(delimiters_left[Delimiters.CODE.value])
+                    < len(delimiters_right[Delimiters.CODE.value])
+                    or len(delimiters_left[Delimiters.CODE.value])
+                    == len(delimiters_right[Delimiters.CODE.value])
                 )
                 and i not in delimiters_right[Delimiters.CODE.value]
             ):
@@ -118,7 +123,8 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
 
             if (
                 node.text[i] == Delimiters.CODE.value
-                and len(delimiters_left[Delimiters.CODE.value]) > len(delimiters_right[Delimiters.CODE.value])
+                and len(delimiters_left[Delimiters.CODE.value])
+                > len(delimiters_right[Delimiters.CODE.value])
                 and i not in delimiters_left[Delimiters.CODE.value]
             ):
                 delimiters_right[Delimiters.CODE.value].append(i)
@@ -133,8 +139,10 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
                         and i - 1 not in delimiters_left[Delimiters.BOLD.value]
                         and i - 1 not in delimiters_right[Delimiters.BOLD.value]
                         and (
-                            len(delimiters_left[Delimiters.BOLD.value]) < len(delimiters_right[Delimiters.BOLD.value])
-                            or len(delimiters_left[Delimiters.BOLD.value]) == len(delimiters_right[Delimiters.BOLD.value])
+                            len(delimiters_left[Delimiters.BOLD.value])
+                            < len(delimiters_right[Delimiters.BOLD.value])
+                            or len(delimiters_left[Delimiters.BOLD.value])
+                            == len(delimiters_right[Delimiters.BOLD.value])
                         )
                     ):
                         delimiters_left[Delimiters.BOLD.value].append(i)
@@ -146,9 +154,10 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
                         and i not in delimiters_left[Delimiters.BOLD.value]
                         and i - 1 not in delimiters_right[Delimiters.BOLD.value]
                         and i - 1 not in delimiters_left[Delimiters.BOLD.value]
-                        and len(delimiters_left[Delimiters.BOLD.value]) > len(delimiters_right[Delimiters.BOLD.value])
+                        and len(delimiters_left[Delimiters.BOLD.value])
+                        > len(delimiters_right[Delimiters.BOLD.value])
                     ):
-                        if delimiters_left[Delimiters.BOLD.value][-1] == i-2:
+                        if delimiters_left[Delimiters.BOLD.value][-1] == i - 2:
                             delimiters_left[Delimiters.BOLD.value].pop()
                         else:
                             delimiters_right[Delimiters.BOLD.value].append(i)
@@ -162,11 +171,25 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
                     and i - 1 not in delimiters_left[Delimiters.BOLD.value]
                     and i - 1 not in delimiters_right[Delimiters.BOLD.value]
                     and (
-                        len(delimiters_left[Delimiters.ITALIC.value]) < len(delimiters_right[Delimiters.ITALIC.value])
-                        or len(delimiters_left[Delimiters.ITALIC.value]) == len(delimiters_right[Delimiters.ITALIC.value])
+                        len(delimiters_left[Delimiters.ITALIC.value])
+                        < len(delimiters_right[Delimiters.ITALIC.value])
+                        or len(delimiters_left[Delimiters.ITALIC.value])
+                        == len(delimiters_right[Delimiters.ITALIC.value])
                     )
                 ):
                     delimiters_left[Delimiters.ITALIC.value].append(i)
+
+                if node.text[i] == "!" and node.text[i + 1] == Delimiters.LINK.value:
+                    delimiters_left[Delimiters.IMAGE.value].append(i)
+
+                if node.text[i] == "]" and node.text[i + 1] == "(":
+                    delimiters_right[Delimiters.LINK_MID.value].append(i)
+
+            if node.text[i] == Delimiters.LINK.value and node.text[i - 1] != "!":
+                delimiters_left[Delimiters.LINK.value].append(i)
+
+            if node.text[i] == Delimiters.LINK_CLOSE.value:
+                delimiters_right[Delimiters.LINK_CLOSE.value].append(i)
 
             if (
                 node.text[i] == Delimiters.ITALIC.value
@@ -176,9 +199,10 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
                 and i not in delimiters_left[Delimiters.BOLD.value]
                 and i - 1 not in delimiters_right[Delimiters.BOLD.value]
                 and i - 1 not in delimiters_left[Delimiters.BOLD.value]
-                and len(delimiters_left[Delimiters.ITALIC.value]) > len(delimiters_right[Delimiters.ITALIC.value])
+                and len(delimiters_left[Delimiters.ITALIC.value])
+                > len(delimiters_right[Delimiters.ITALIC.value])
             ):
-                if delimiters_left[Delimiters.ITALIC.value][-1] == i-1:
+                if delimiters_left[Delimiters.ITALIC.value][-1] == i - 1:
                     delimiters_left[Delimiters.ITALIC.value].pop()
                     if i + 1 < len(node.text):
                         if node.text[i + 1] != " ":
@@ -186,24 +210,50 @@ def split_nodes_delimiter(old_nodes: Sequence[TextNode], delimiter: str, text_ty
                 else:
                     delimiters_right[Delimiters.ITALIC.value].append(i)
 
-            '''
-                if node.text[i] == "!" and node.text[i + 1] == Delimiters.LINK.value:
-                    delimiters[Delimiters.IMAGE.value].append(i)
+        # for i in range(len(delimiters_left[Delimiters.CODE.value])):
+        print(node.text)
+        while len(delimiters_left[Delimiters.CODE.value]) > 0:
+            start = delimiters_left[Delimiters.CODE.value].pop(0)
+            stop = delimiters_right[Delimiters.CODE.value].pop(0)
+            res.append(
+                TextNode(
+                    node.text[start + 1 : stop],
+                    TextType.TEXT_CODE,
+                )
+            )
+            # stop + 1 - start
+            for delimiters in delimiters_left:
+                i = -1
+                delimiter: int
+                for delimiter in reversed(delimiters_left[delimiters]):
+                    # print("testing", delimiter, start)
+                    if delimiter > start:
+                        # print(delimiter - (stop + 1 - start))
+                        if delimiter - (stop + 1 - start) >= 0:
+                            delimiters_left[delimiters][i] = delimiter - (
+                                stop + 1 - start
+                            )
+                            print("hi:", delimiters_left[delimiters][i], i, delimiter)
+                            # print(delimiters_left)
+                    else:
+                        i = i - 1
+                        # print(delimiter, i)
+                        continue
+                    i = i - 1
+            print("start ---", node.text[:start])
+            print("stop ---", node.text[stop + 1 :])
+            node.text = node.text[:start] + node.text[stop + 1 :]
+            print(node.text)
 
-                if node.text[i] == Delimiters.LINK.value and node.text[i - 1] != "!":
-                    delimiters[Delimiters.LINK.value].append(i)
+        while len(delimiters_left[Delimiters.CODE.value]) > 0:
+            res.append(
+                TextNode(
+                    node.text[
+                        delimiters_left[Delimiters.CODE.value].pop(0)
+                        + 1 : delimiters_right[Delimiters.CODE.value].pop(0)
+                    ],
+                    TextType.TEXT_CODE,
+                )
+            )
 
-                if node.text[i] == "]" and node.text[i + 1] == "(":
-                    delimiters[Delimiters.LINK_MID.value].append(i)
-
-            if node.text[i] == Delimiters.LINK_CLOSE.value:
-                delimiters[Delimiters.LINK_CLOSE.value].append(i)
-
-            if i == len(node.text) -1:
-                if node.text[i] == Delimiters.ITALIC.value and node.text[i-1] != Delimiters.ITALIC.value:
-                    delimiters[Delimiters.ITALIC.value].append(i)
-            if i - 1 >= 0:
-            '''
-
-            #i += 1
-    print("left:", delimiters_left,"\nright:", delimiters_right)
+    print("left:", delimiters_left, "\nright:", delimiters_right)
